bed0a377617d1284cfd52775e8455456935bce4e
TheFakeElon <59686430+TheFakeElon@users.noreply.github.com>
2025-10-18 21:19:34 +1100

    guh

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime
 import random
 import schedule
 import pytz
+import lavalink
 from discord.ext import commands
 
 if not os.path.isdir('store'):
@@ -18,13 +19,16 @@ config = {}
 with open('store/config.json', 'r') as f:
 	config = json.load(f)
 
-timezone = pytz.timezone('Australia/Melbourne')
-conversation_log_interval = datetime.timedelta(seconds=15) # Minimum time between logging a message for conversation
-conversation_response_interval = datetime.timedelta(seconds=240) # Minimum time between conversation responses
-conversation_response_chance = 0.2 # Chance to respond per message after response cooldown (0-1)
+# shhhh
+poison = ''
+
+timezone = pytz.timezone(config['timezone'])
+conversation_log_interval = datetime.timedelta(seconds=config['conversation_log_interval']) # Minimum time between logging a message for conversation
+conversation_response_interval = datetime.timedelta(seconds=config['conversation_response_interval']) # Minimum time between conversation responses
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.dm_messages = True
 intents.members =  True
 intents.guilds = True
 intents.voice_states = True
@@ -178,12 +182,13 @@ def conversation_catalog(message: discord.Message, force: bool = False):
 	"""Record messages ocassionally to randomly respond with"""
 	now = datetime.datetime.now()
 	if force or message.guild.id not in clog_next_record or clog_next_record[message.guild.id] < now:
-		if message.mention_everyone:
-			return
+		# uncomment if gay
+		#if message.mention_everyone:
+		#	return
 		msg = ''
 		if len(message.content) != 0 and len(message.content) < 200:
 			msg = message.content
-		elif message.attachments and message.attachments[0].content_type and message.attachments[0].content_type.startswith('image/') and message.attachments[0].url:
+		elif message.attachments and message.attachments[0].content_type and (message.attachments[0].content_type.startswith('image/') or message.attachments[0].content_type.startswith('video/')) and message.attachments[0].url:
 			msg = message.attachments[0].url
 		elif message.embeds and message.embeds[0].url:
 			msg = message.embeds[0].url
@@ -194,15 +199,18 @@ def conversation_catalog(message: discord.Message, force: bool = False):
 			f.write('\n')
 		clog_next_record[message.guild.id] = now + conversation_log_interval
 
+res_chance = config['conversation_response_chance']
 clog_next_response = {}
 def conversation_response(message: discord.Message) -> str | None:
 	"""Chance to respond to this message with a random message (return value)"""
 	now = datetime.datetime.now()
-	if message.guild.id not in clog_next_response or clog_next_response[message.guild.id] < now:
-		chance = conversation_response_chance
-		if bot.user in message.mentions:
-			chance = min(conversation_response_chance * 3, 1)
-		if random.random() <= chance and os.path.exists('store/conversation.txt'):
+	if message.guild.id not in clog_next_response or clog_next_response[message.guild.id] < now or bot.user in message.mentions:
+		if (random.random() <= res_chance or bot.user in message.mentions) and os.path.exists('store/conversation.txt'):
+			global poison
+			if len(poison):
+				r = poison
+				poison = ''
+				return r
 			clog_next_response[message.guild.id] = now + conversation_response_interval
 			with open('store/conversation.txt', 'r', encoding='utf-8') as f:
 				return random.choice(f.readlines()).replace('\\n', '\n')
@@ -214,11 +222,25 @@ def conversation_response(message: discord.Message) -> str | None:
 
 @bot.event
 async def on_ready():
-	schedule.every().monday.at('00:00', timezone).do(reset_weekly_leaderboard)
 	print(f'Logged in as {bot.user}')
+	schedule.every().monday.at('00:00', timezone).do(reset_weekly_leaderboard)
+	if config['lavalink_enable'] and not hasattr(bot, 'lavalink'):
+		bot.lavalink = lavalink.Client(bot.user.id)
+		bot.lavalink.add_node(config['lavalink_host'],
+						config['lavalink_port'],
+						config['lavalink_password'],
+						config['lavalink_region'],
+						config['lavalink_name'])
 
 @bot.event
 async def on_voice_state_update(member, before, after):
+	# lavalink
+	if member.id == bot.user.id and before.channel and not after.channel:
+		# Bot was disconnected from voice
+		player = bot.lavalink.get_player(member.guild.id)
+		if player:
+			await player.destroy()
+	# leaderboard
 	guild_id = member.guild.id
 	user_id = member.id
 
@@ -259,13 +281,18 @@ async def on_voice_state_update(member, before, after):
 async def on_message(message: discord.Message):
 	if(message.author.id == bot.user.id):
 		return
+	if(isinstance(message.channel, discord.DMChannel)):
+		if(message.author.id == config['dev_user_id']):
+			global poison
+			poison = message.content
+		return
 	if(message.content and message.content[0] == '!'):
 		await bot.process_commands(message)
 		return
 	conversation_catalog(message)
 	response = conversation_response(message)
 	if response:
-		await message.channel.send(content=response)
+		await message.channel.send(response)
 	
 
 @bot.command()
@@ -275,16 +302,15 @@ async def leaderboard(ctx: commands.Context, category: str = 'current'):
 	if embed:
 		await ctx.send(embed=embed)
 	else:
-		await ctx.send(content='Unknown category. Choose from current|top|total.')
+		await ctx.send('Unknown category. Choose from current|top|total.')
 
 @bot.command()
 async def lbchannel(ctx: commands.Context, channel: discord.TextChannel):
 	"""Set weekly VC activity leaderboard report channel"""
 	if ctx.author.guild_permissions.manage_channels == False:
-		await ctx.send(content='Insufficient permissions.')
-		return
+		return await ctx.send('Insufficient permissions.')
 	msg = set_leaderboard_channel(ctx.guild, channel)
-	await ctx.send(content=msg)
+	await ctx.send(msg)
 
 @bot.command(aliases=['spin', 'wheelspin'])
 async def wheel(ctx: commands.Context):
@@ -297,11 +323,9 @@ async def wheel(ctx: commands.Context):
 	elif ctx.message.mention_everyone or 'everyone' in ctx.message.content:
 		selected = random.choice(ctx.channel.members)
 	else:
-		await ctx.send(content='Invalid command usage. Must mention users in command argument or be in a voice channel.')
-		return
+		return await ctx.send('Invalid command usage. Must mention users in command argument or be in a voice channel.')
 	if selected is None:
-		await ctx.send(content='Error. No valid users found, contact dev.')
-		return
+		return await ctx.send('Error. No valid users found, contact dev.')
 	await ctx.send(f'<@{selected.id}>')
 
 @bot.command(aliases=['flip', 'coin'])
@@ -309,27 +333,56 @@ async def coinflip(ctx: commands.Context):
 	"""Flip a coin"""
 	outcomes = [':flushed:\nHeads', ':snake:\nTails', 'Woah - It landed completely upright!?']
 	probs = [49.95, 49.95, 0.1]
-	await ctx.send(content=random.choices(outcomes, probs, k=1)[0])
+	await ctx.send(random.choices(outcomes, probs, k=1)[0])
 	
 @bot.command()
 async def catchup(ctx: commands.Context, limit = 5000):
 	"""Goes through previous channel history to update quote DB. argument specifies how far to look back (default=5000)"""
 	if ctx.author.guild_permissions.administrator == False:
-		await ctx.send(content='Insufficient permissions')
-		return
-	await ctx.send(content='Gathering quotes...')
+		return await ctx.send('Insufficient permissions')
+	await ctx.send('Gathering quotes...')
 	async for msg in ctx.channel.history(limit=limit):
 		conversation_catalog(msg, True)
-	await ctx.send(content=f'Finished. Gathered ~{limit} quotes.')\
+	await ctx.send(f'Finished. Gathered ~{limit} quotes.')\
 
 @bot.command()
 async def update(ctx: commands.Context):
 	"""Updates bot from github"""
 	if ctx.author.id != config['dev_user_id']:
-		await ctx.send(content='no')
-		return
-	await ctx.send(content='Oughh, I\'m updating!!')
+		return await ctx.send('no')
+	await ctx.send('Beginning update and restart.')
+	if(config['restart_special_message']):
+		await ctx.send(config['restart_special_message'])
 	restart_bot()
-	await ctx.send(content='Update failed.')
-	
+	await ctx.send('Update failed.')
+
+@bot.command()
+async def play(ctx: commands.Context, *, query):
+	"""Play music via link/search query"""
+	if not config['lavalink_enable']:
+		return await ctx.send('Music not enabled in configuration')
+	player = bot.lavalink.get_player(ctx.guild.id)
+	if player is None:
+		return await ctx.send('Music player not initialized')
+	if not player.is_connected:
+		if not ctx.author.voice:
+			return await ctx.send("You are not in a voice channel")
+		channel = ctx.author.voice.channel
+		await channel.connect(cls=lavalink.discord.VoiceClient)
+		player.store('channel', channel.id)
+	elif player.channel_id == ctx.channel.id:
+		player.move_to(ctx.author.voice.channel.id)
+	results = await player.search(query, ctx.author)
+	if not results or not results.tracks:
+		return await ctx.send("No results found.")
+	track = results.tracks[0]
+	player.add(requester=ctx.author.id, track=track)
+	channel.store('requester_id', ctx.author.id)
+	if not player.is_playing:
+		await player.play()
+		await ctx.send(f"Now playing: {track.title}")
+	else:
+		await ctx.send(f"Added to queue: {track.title}")
+
+
 bot.run(config['token'])
